b70fec0d8c4991c69feac49523d6fe403d771a93
lalaalal <bontin0128@gmail.com>
2021-05-21 03:49:16 +0900

    Add MccCalendarView, Implement CalendarUIHandler::updateUI()

--- app/src/main/java/com/mcc/integrated/CalendarActivity.java
+++ app/src/main/java/com/mcc/integrated/CalendarActivity.java
@@ -5,21 +5,26 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.mcc.integrated.calendar.CalendarClient;
+import com.mcc.integrated.calendar.MccCalendarView;
 import com.mcc.integrated.calendar.Todo;
 
 import java.util.ArrayList;
 
 public class CalendarActivity extends AppCompatActivity {
+    private MccCalendarView mccCalendarView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        mccCalendarView = findViewById(R.id.mcc_calendar_view);
     }
 
     public class CalendarUIHandler {
 
         public void updateUI(ArrayList<Todo> todoList) {
-            // TODO: 5/21/21 update ui with todo list 
+            mccCalendarView.setTodoList(todoList);
         }
     }
 }
