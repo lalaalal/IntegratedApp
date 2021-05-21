41054f926f2ec6bef20b293d0cafc76f476b897f
lalaalal <bontin0128@gmail.com>
2021-05-22 06:23:49 +0900

    Get id, pw from intent

--- app/src/main/java/com/mcc/integrated/CalendarActivity.java
+++ app/src/main/java/com/mcc/integrated/CalendarActivity.java
@@ -1,9 +1,9 @@
 package com.mcc.integrated;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.mcc.integrated.calendar.CalendarClient;
 import com.mcc.integrated.calendar.MccCalendarView;
@@ -12,8 +12,8 @@ import com.mcc.integrated.calendar.Todo;
 import java.util.ArrayList;
 
 public class CalendarActivity extends AppCompatActivity {
-    private static final String KEY_BUNDLE_ID = "id";
-    private static final String KEY_BUNDLE_PW = "pw";
+    public static final String KEY_EXTRA_ID = "id";
+    public static final String KEY_EXTRA_PW = "pw";
 
     private MccCalendarView mccCalendarView;
     private TextView loadStatusTv;
@@ -26,8 +26,9 @@ public class CalendarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-        id = savedInstanceState.getString(KEY_BUNDLE_ID);
-        pw = savedInstanceState.getString(KEY_BUNDLE_PW);
+        Intent intent = getIntent();
+        id = intent.getStringExtra(KEY_EXTRA_ID);
+        pw = intent.getStringExtra(KEY_EXTRA_PW);
 
         mccCalendarView = findViewById(R.id.mcc_calendar_view);
         loadStatusTv = findViewById(R.id.load_status_tv);
